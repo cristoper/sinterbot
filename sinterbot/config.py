e1ab05a27de6744eab2a67353f84d2ba133cbf9c
A. Cynic <331548+cristoper@users.noreply.github.com>
2020-12-02 14:58:09 -0700

    Implement validate() on ConfFile
    It raises exceptions if the conffile is inconsistent (contains black
    list members who are not listed as santas, etc.)
    
    TODO: check that the number of possible derangements given constraints
    is greater than number of santas.

--- sinterbot/config.py
+++ sinterbot/config.py
@@ -2,18 +2,27 @@ import os
 import re
 import logging
 from typing import List, Tuple
+# TODO enable/disable logging
 log = logging.getLogger(__name__)
 
 try:
     import pathlib
 except ImportError as e:
-    log.error("Can't import pathlib. Make sure you have python version >= 3.4.")
+    log.error("Can't import pathlib. Make sure you have python version >= 3.4")
     raise e
 
 class ParseError(Exception):
     """Used for exceptions raised during parsing"""
     def __init__(self, lineno: int):
         self.line = lineno
+
+class ValidateError(Exception):
+    """Used for exceptions during validation"""
+    def __init__(self, msg: str = ""):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
 
 class Santa:
     def __init__(self, name, email):
@@ -23,14 +32,29 @@ class Santa:
     def __repr__(self):
         return "%s %s <%s>" % (self.__class__, self.name, self.email)
 
+class SantaList:
+    def __init__(self, santas: List[Santa] = None):
+        if santas is None: santas = []
+        self.santas = santas
+
+    def emails(self):
+        """Return list of all santa emails"""
+        emails = []
+        for santa in self.santas:
+            emails.append(santa.email)
+        return emails
+
+    def add(self, santa):
+        self.santas.append(santa)
+
 Blacklist_T = List[Tuple[str, str]]
 class Blacklist:
     def __init__(self):
-        self._bl: Blacklist_T = []
+        self.list: Blacklist_T = []
 
     def __repr__(self):
         s = "%s: {" % self.__class__
-        for i, pair in enumerate(self._bl):
+        for i, pair in enumerate(self.list):
             if i > 0:
                 s += ", "
             s += "(" + ", ".join(pair) + ")"
@@ -38,25 +62,55 @@ class Blacklist:
         return s
 
     def add_emails(self, emails: Tuple[str, str]):
-        self._bl.append(emails)
-
-    def get_bl(self) -> Blacklist_T:
-        return self._bl
+        self.list.append(emails)
 
 class ConfFile:
-
     def __init__(self, path: str):
         self.path = pathlib.Path(path).expanduser()
 
         # Set defaults
         self.m = 2
-        self.santas: List[Santa] = []
+        self.santas = SantaList()
         self.bl = Blacklist()
         self.smtpport = 587
         self.smtppass = os.environ.get("sinter_smtp_pass")
-        self.__parse()
 
-    def __parse(self):
+    @staticmethod
+    def parse_and_validate(path: str):
+        """Factory which parses and validates the config file at path"""
+        c = ConfFile(path)
+        c.parse()
+        c.validate()
+        return c
+
+    def validate(self):
+        """
+        Raises an exception of type ValidateError (with informative __str__) if
+        this ConfFile fails its consistency checks.
+        """
+        if len(self.santas.santas) < 2:
+            raise ValidateError("Config file must list at least two santas")
+
+        # make sure all santas have unique email addresses
+        emails = self.santas.emails()
+        unique_emails = set(emails)
+        if len(emails) != len(unique_emails):
+            raise ValidateError("All email addresses must be unique")
+
+        # make sure the black list contains only email addresses listed as santas
+        for pair in self.bl.list:
+            for email in pair:
+                if email.casefold() not in self.santas.emails():
+                    raise ValidateError("Black list contains email not listed in santas: %s" % email)
+
+        # TODO: validate constraints allow for at least 1 valid derangement!
+
+
+    def parse(self):
+        """Parses the file at self.path and populates instance variables.
+
+        Does not validate settings (for that see `validate()`)
+        """
 
         CommentPat = re.compile(r'^\s*#.*')
         LinePat = re.compile(r'^\s*(.+):\s*(.+)\s*')
@@ -103,5 +157,5 @@ class ConfFile:
                     self.bl.add_emails((first, second))
                 else:
                     # no pre-defined prefix, assume this is a santa name
-                    self.santas.append(Santa(g[0], val))
+                    self.santas.add(Santa(g[0], val))
 
--- sinterbot/test/test_config.py
+++ sinterbot/test/test_config.py
@@ -3,19 +3,21 @@ import sinterbot.config
 
 class TestParse(unittest.TestCase):
     def test_successful_parse(self):
-        c = sinterbot.config.ConfFile('sample.conf')
+        c = sinterbot.config.ConfFile.parse_and_validate('sample.conf')
         self.assertEqual(c.smtpserver, 'smtp.email.tld')
         self.assertEqual(c.smtpuser, 'smtpuser@email.tld')
         self.assertEqual(c.smtppass, 'secret')
         self.assertEqual(c.smtpport, '587')
         self.assertEqual(int(c.m), 1)
-        self.assertEqual(len(c.bl.get_bl()), 2)
-        self.assertEqual(len(c.santas), 5)
+        self.assertEqual(len(c.bl.list), 2)
+        self.assertEqual(len(c.santas.santas), 5)
+        self.assertEqual(len(c.santas.emails()), 5)
 
     def test_missing_colon(self):
         """Test that malformed config file raises exception"""
         with self.assertRaises(sinterbot.config.ParseError) as err:
-            sinterbot.config.ConfFile('sinterbot/test/malformed.conf')
+            c = sinterbot.config.ConfFile('sinterbot/test/malformed.conf')
+            c.parse()
         self.assertEqual(err.exception.line, 7)
 
 
